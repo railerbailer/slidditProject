6a87450342ff4d7eed415ef0e1916193b3da8d99
railerbailer <rasmus.arvidsson1@gmail.com>
2019-04-25 19:28:57 +0200

    remade switchcatbuttons

--- src/components/switchCategoryButtons.js
+++ src/components/switchCategoryButtons.js
@@ -1,17 +1,19 @@
-import React from "react";
-const SwitchCategoryButtons = props => {
-  const { isSearchActivated, collectionsMode, showListInput, isModalVisible, switchCat } = props;
-  const noInputsActivated = !isSearchActivated && !showListInput && !isModalVisible;
-  return (
-    <button ref={button => button && noInputsActivated && button.focus()} className={`iconRight`}>
-      <i onClick={switchCat} className="material-icons">
-        shuffle
-      </i>
-      <p onClick={switchCat}>
-        Shuffle <br />
-        {collectionsMode ? "collection" : "subreddit"}
-      </p>
-    </button>
-  );
-};
+import React, { Component } from "react";
+class SwitchCategoryButtons extends Component {
+  render() {
+    const { isSearchActivated, collectionsMode, showListInput, isModalVisible, switchCat } = this.props;
+    const noInputsActivated = !isSearchActivated && !showListInput && !isModalVisible;
+    return (
+      <button ref={button => button && noInputsActivated && button.focus()} className={`iconRight`}>
+        <i onClick={switchCat} className="material-icons">
+          shuffle
+        </i>
+        <p onClick={switchCat}>
+          Shuffle <br />
+          {collectionsMode ? "collection" : "subreddit"}
+        </p>
+      </button>
+    );
+  }
+}
 export default SwitchCategoryButtons;
